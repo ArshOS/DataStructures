e4961a9a027249a56b0b5b174b83f1e0995f0c7e
ArshOS <arshadjamal1996@gmail.com>
2023-06-06 19:15:11 +0530

    Implement ConnectedComponents.java

--- Graphs/ConnectedComponents.java
+++ Graphs/ConnectedComponents.java
@@ -2,11 +2,45 @@ import java.util.*;
 
 public class ConnectedComponents {
 
-    private static ArrayList<ArrayList<Integer>> getConnectedComponents(int[][] mat, int v) {
-
+    /**
+     * @param mat
+     * @param v
+     * @param src
+     * @param visited
+     * @param list
+     */
+    private static void getConnectedComponentsHelper(int[][] mat, int v, int src, boolean[] visited, ArrayList<Integer> list) {
         
+        visited[src] = true;
+        list.add(src);
 
-        return null;
+        for(int i=0; i<v; i++) {
+            if(mat[src][i] == 1 && !visited[i]) {
+                getConnectedComponentsHelper(mat, v, i, visited, list);
+
+            }
+        }
+    }
+
+    /**
+     * @param mat
+     * @param v
+     * @return
+     */
+    private static ArrayList<ArrayList<Integer>> getConnectedComponents(int[][] mat, int v) {
+        boolean[] visited = new boolean[v];
+
+        ArrayList<ArrayList<Integer>> components = new ArrayList<>();
+
+        for(int i=0; i<v; i++) {
+            if(!visited[i]) {
+                ArrayList<Integer> list = new ArrayList<>();
+                getConnectedComponentsHelper(mat, v, i, visited, list);
+                components.add(list);
+            }
+        }
+
+        return components;
     }
     
     public static void main(String[] args) {
@@ -25,6 +59,10 @@ public class ConnectedComponents {
         s.close();
 
         ArrayList<ArrayList<Integer>> components = getConnectedComponents(mat, V);
+
+        for(ArrayList<Integer> list : components) {
+            System.out.println(list.toString());
+        }
     }
     
 }
