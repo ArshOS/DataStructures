1ccda551311cf37fe6377381e2a3ba3d71599665
ArshOS <arshadjamal1996@gmail.com>
2023-06-06 15:56:33 +0530

    Implement getPathDFS

--- Graphs/GetPath.java
+++ Graphs/GetPath.java
@@ -2,11 +2,36 @@ import java.util.*;
 
 public class GetPath {
 
-    private static ArrayList<Integer> getPath(int[][] mat, int v, int sv, int ev) {
+    private static ArrayList<Integer> getPathDFSHelper(int[][] mat, int v, int sv, int ev, boolean[] visited) {
+        
+        if(sv == ev) {
+            ArrayList<Integer> baseAns = new ArrayList<>();
+            baseAns.add(sv);
+            return baseAns;
+        }
 
-        // TO:DO
+        visited[sv] = true;
+        
+        for(int i=0; i<v; i++) {
+            if(mat[sv][i] == 1 && !visited[i]) {
+                ArrayList<Integer> smallAns = getPathDFSHelper(mat, v, i, ev, visited);
 
+                if(smallAns != null) {
+                    smallAns.add(sv);
+                    return smallAns;
+                }
+            }
+            
+        }
         return null;
+    }
+
+    private static ArrayList<Integer> getPathDFS(int[][] mat, int v, int sv, int ev) {
+
+        boolean[] visited = new boolean[v];
+
+        return getPathDFSHelper(mat, v, sv, ev, visited);
+        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -28,7 +53,14 @@ public class GetPath {
 
         sc.close();
 
-        System.out.println(getPath(mat, V, sv, ev).toString());
+        ArrayList<Integer> path = getPathDFS(mat, V, sv, ev);
+        
+        if(path == null) {
+            System.out.println("Sorry!!! No path found.");
+        }
+        else {
+            System.out.println(path.toString());
+        }
     }
 
 }
